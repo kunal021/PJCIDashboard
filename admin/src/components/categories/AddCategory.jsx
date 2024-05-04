0964cb58e31b260c92fdaf7fef5b02e43068394a
Kunal Singh <ks820177@gmail.com>
2024-05-04 14:15:00 +0530

    update

--- admin/src/components/categories/AddCategory.jsx
+++ admin/src/components/categories/AddCategory.jsx
@@ -4,7 +4,7 @@ import { useDispatch } from "react-redux";
 import { addCategory } from "../../redux/categories/categorySlice";
 import axios from "axios";
 
-function AddCategory({ fetchCategory }) {
+function AddCategory({ fetchCategory, setAddNewCategory }) {
   const [categoryName, setCategoryName] = useState("");
   const dispatch = useDispatch();
 
@@ -22,13 +22,15 @@ function AddCategory({ fetchCategory }) {
       console.error("Error fetching courses:", error);
     }
     setCategoryName("");
+    setAddNewCategory((perv) => !perv);
   };
 
   return (
-    <div className="container mx-auto w-[20vw]">
+    <div className="container flex flex-col justify-center items-center">
+      <h1 className="text-center text-3xl font-bold">Add Category</h1>
       <form
         onSubmit={handleSubmit}
-        className="flex flex-col justify-between items-center border-2 rounded-lg border-gray-500 border-dashed p-3 w-full md:w-auto"
+        className="flex flex-col justify-between items-center border-2 rounded-lg border-gray-900 p-3 w-full my-10 md:w-auto"
       >
         <input
           type="text"
@@ -36,15 +38,21 @@ function AddCategory({ fetchCategory }) {
           value={categoryName}
           onChange={(e) => setCategoryName(e.target.value)}
           placeholder="Enter Category Name"
-          className="border-2 rounded-lg border-black p-2 text-lg font-bold mb-4 w-full md:w-auto"
+          className="border-2 rounded-lg border-black p-2 text-sm font-bold mb-4 w-full md:w-auto"
         />
         <button
           type="submit"
-          className="border-2 rounded-lg border-blue-500 p-2 text-lg font-semibold text-blue-500 hover:border-transparent hover:bg-blue-500 hover:text-white transition-all duration-500 w-full md:w-auto"
+          className="border-2 rounded-lg border-blue-500 p-2 text-sm font-semibold text-blue-500 hover:border-transparent hover:bg-blue-500 hover:text-white transition-all duration-500 w-full md:w-auto"
         >
           Add Category
         </button>
       </form>
+      <button
+        onClick={() => setAddNewCategory((perv) => !perv)}
+        className="border-2 rounded-lg border-red-500 py-2 px-4 text-sm font-semibold text-red-500 hover:border-transparent hover:bg-red-500 hover:text-white transition-all duration-500 w-full md:w-auto"
+      >
+        Close
+      </button>
     </div>
   );
 }
--- admin/src/components/categories/GetCategory.jsx
+++ admin/src/components/categories/GetCategory.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import {
   setCategory,
@@ -21,6 +21,8 @@ const fetchCategory = async (dispatch) => {
 };
 
 function GetCategory() {
+  const [addNewCategory, setAddNewCategory] = useState(false);
+
   const dispatch = useDispatch();
   const category = useSelector((state) => state.category.category);
 
@@ -45,48 +47,67 @@ function GetCategory() {
 
   return (
     <div className="container w-[85%] flex flex-col justify-center items-center mx-5">
-      <h1 className="text-center text-3xl font-bold my-5">Category List</h1>
-      <table className="table-auto w-full m-5 border-2">
-        <thead>
-          <tr>
-            <th className="p-2 text-sm">Id</th>
-            <th className="p-2 text-sm">Name</th>
-            <th className="p-2 text-sm">Update</th>
-            <th className="p-2 text-sm">Delete</th>
-          </tr>
-        </thead>
-        <tbody className="text-center">
-          {category.map((item) => (
-            <tr key={item.id}>
-              {console.log(item.id)}
-              <td className="border p-2 text-sm">{item.id}</td>
-              <td className="border p-2 text-sm">{item.name}</td>
-              <td className="border p-2 text-sm">
-                <button
-                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                  // onClick={() => handleUpdate(item.name, item.id)}
-                >
-                  Update
-                </button>
-              </td>
-              <td className="border p-2 text-sm">
-                <button
-                  className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
-                  onClick={() => handleDelete(item.id)}
-                >
-                  Delete
-                </button>
-              </td>
-              <td className="border p-2 text-sm">
-                <Link to={`/get-course-category-wise?id=${item.id}`}>
-                  See all Course
-                </Link>
-              </td>
+      <div
+        className={`${
+          addNewCategory
+            ? "hidden"
+            : "container w-[85%] flex flex-col justify-center items-center mx-5"
+        } `}
+      >
+        <h1 className="text-center text-3xl font-bold my-5">Category List</h1>
+        <table className="table-auto w-full m-5 border-2">
+          <thead>
+            <tr>
+              <th className="p-2 text-sm">Id</th>
+              <th className="p-2 text-sm">Name</th>
+              <th className="p-2 text-sm">Update</th>
+              <th className="p-2 text-sm">Delete</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-      <AddCategory fetchCategory={() => fetchCategory(dispatch)} />
+          </thead>
+          <tbody className="text-center">
+            {category.map((item) => (
+              <tr key={item.id}>
+                {console.log(item.id)}
+                <td className="border p-2 text-sm">{item.id}</td>
+                <td className="border p-2 text-sm">{item.name}</td>
+                <td className="border p-2 text-sm">
+                  <button
+                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                    // onClick={() => handleUpdate(item.name, item.id)}
+                  >
+                    Update
+                  </button>
+                </td>
+                <td className="border p-2 text-sm">
+                  <button
+                    className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+                    onClick={() => handleDelete(item.id)}
+                  >
+                    Delete
+                  </button>
+                </td>
+                <td className="border p-2 text-sm">
+                  <Link to={`/get-course-category-wise?id=${item.id}`}>
+                    See all Course
+                  </Link>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <button
+          onClick={() => setAddNewCategory((prev) => !prev)}
+          className="border-2 rounded-lg border-blue-500 p-2 text-sm font-semibold text-blue-500 hover:border-transparent hover:bg-blue-500 hover:text-white transition-all duration-500 w-full md:w-auto"
+        >
+          Add Category
+        </button>
+      </div>
+      {addNewCategory && (
+        <AddCategory
+          fetchCategory={() => fetchCategory(dispatch)}
+          setAddNewCategory={setAddNewCategory}
+        />
+      )}
     </div>
   );
 }
