ffb5a3637b8092051e1ae08f4c53eca62fa4868a
Kunal Singh <ks820177@gmail.com>
2024-07-07 18:22:31 +0530

    update

--- admin/src/components/course/AddVideoInCourse.jsx
+++ admin/src/components/course/AddVideoInCourse.jsx
@@ -59,8 +59,8 @@ function AddVideoInCourse() {
       </Dialog.Trigger>
       <Dialog.Portal>
         <Dialog.Overlay className="bg-blackA6 data-[state=open]:animate-overlayShow fixed inset-0" />
-        <Dialog.Content className="data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
-          <Dialog.Title className="text-mauve12 m-0 text-[17px] font-medium">
+        <Dialog.Content className="data-[state=open]:animate-contentShow fixed top-[50%] overflow-auto left-[50%] max-h-[95vh] w-[90vw] max-w-[550px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
+          <Dialog.Title className="text-mauve12 m-0 text-2xl font-bold">
             Add Video
           </Dialog.Title>
           {loading ? (
@@ -80,11 +80,13 @@ function AddVideoInCourse() {
                           key={idx}
                           className="flex justify-center items-center font-medium w-full border rounded-md border-zinc-300 ml-2 my-2 p-3 gap-3"
                         >
-                          <div className="flex flex-col justify-center items-start gap-4 w-[85%]">
+                          <div className="flex flex-col justify-center items-start gap-4 w-[90%]">
                             <div className="flex justify-start items-center text-sm w-full gap-4">
-                              <Avatar className="bg-gray-500 text-white">
-                                {idx + 1}
-                              </Avatar>
+                              <div className="flex justify-center items-center">
+                                <Avatar className="bg-gray-500 text-white w-10">
+                                  {idx + 1}
+                                </Avatar>
+                              </div>
                               {/* <div>Video ID: {item.video_id}</div> */}
                               <div className="flex flex-wrap text-wrap">
                                 {typeof item.video_title == "string"
