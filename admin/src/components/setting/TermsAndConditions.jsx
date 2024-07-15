bf679b8f3c1d48f960f42b15ba6d217c0d6766d1
Kunal Singh <ks820177@gmail.com>
2024-07-15 17:39:10 +0530

    update

--- admin/src/components/setting/TermsAndConditions.jsx
+++ admin/src/components/setting/TermsAndConditions.jsx
@@ -64,7 +64,7 @@ function TermsAndConditions() {
           </div>
           <button
             onClick={handlleUpdate}
-            className="bg-blue-500 hover:bg-blue-700 text-white w-[20%] font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            className="bg-blue-50 hover:bg-blue-100 text-black border-blue-200 w-[20%] font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
           >
             Update
           </button>
